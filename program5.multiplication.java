c1b6274e709a954441b2666aa471dcc47e2d023b
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-20 16:37:17 +0530

    Create program5.multiplication.java

---
+++ program5.multiplication.java
@@ -0,0 +1,13 @@
+ import java.util.Scanner;
+ public class multiplication{
+   public static void main(String[] args){
+     Scanner mul = new Scanner(System.in);
+     
+     double num1 = mul.nextDouble();
+     double num2 = mul.nextDouble();
+     
+     double m = num1 * num2;
+     
+     System.out.println(m);
+   }
+ }
