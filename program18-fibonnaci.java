2ba9a01bb8f3056634cff263d9ab3e37d17e6420
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-20 23:40:37 +0530

    Create program18-fibonnaci.java

---
+++ program18-fibonnaci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+
+        int firstTerm = 0, secondTerm = 1;
+
+        System.out.print("Fibonacci Series: " + firstTerm + ", " + secondTerm);
+
+        for (int i = 3; i <= n; i++) {
+            int nextTerm = firstTerm + secondTerm;
+            System.out.print(", " + nextTerm);
+
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
