f8d7e86ab8fc4ad171c9c9c200d35ab0172e1878
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-20 16:57:22 +0530

    Create program8-float multiplication.java

---
+++ program8-float multiplication.java
@@ -0,0 +1,12 @@
+ import java.util.Scanner;
+ public class division{
+   public static void main(String[] args){
+     Scanner mul = new Scanner(System.in);
+     
+     float num1 = mul.nextFloat();
+     float num2 = mul.nextFloat();
+     
+     float q = num1 * num2;
+     System.out.println(q);
+   }
+ }
