9805eac9c28cfdd15d8692f7126b9a27b5cd8ca7
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-20 16:36:30 +0530

    Create program4-addition by user.java

---
+++ program4-addition by user.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class addition{
+  public static void main(String[] args){
+    Scanner add = new Scanner(System.in);
+    double n1 = add.nextDouble;
+    double n2 = add.nextDouble;
+    double addition = n1 + n2;
+    System.out.println(addition);
+  }
+}
