3b13d2aab75dd402801d41cf15757dfb50b41c58
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-20 16:29:22 +0530

    Create program 3- subtraction.java

---
+++ program 3- subtraction.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ public class Addition{
+   public static void main(String[] args){
+     Scanner sub = new Scanner(System.in);
+     
+     double num1 = sub.nextDouble();
+     double num2 = sub.nextDouble();
+     
+     double s = num1 - num2;
+     
+     System.out.println(s);
+   }
+ }
