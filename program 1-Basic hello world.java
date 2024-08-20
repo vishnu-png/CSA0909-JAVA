f77ac61b6ddfb043ed646799e387447b1bb5dfc8
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-20 16:16:46 +0530

    Create program 1-Basic hello world.java

---
+++ program 1-Basic hello world.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+ public class print{
+   public static void main(String[] args){
+     Scanner word = new Scanner(System.in);
+     System.out.println("Hello world");
+   }
+ }
