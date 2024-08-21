ed113be125fdd836322c44c5281259e185ac69b3
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-21 20:40:48 +0530

    Create program28-half pyramid pattern.java

---
+++ program28-half pyramid pattern.java
@@ -0,0 +1,17 @@
+public class pattern{
+  public static void main(String[] args){
+    int i,j;
+    for(i = 1 ; i <= 4; i++){
+      for(j = 1; j <= i; j++){
+        System.out.print(i+" ");
+      }
+      System.out.println();
+    }
+      for(i = 3; i >= 0 ; i--){
+        for(j = 1; j <= i; j++){
+          System.out.print(i+" ");
+        }
+        System.out.println();
+      }
+    }
+}
