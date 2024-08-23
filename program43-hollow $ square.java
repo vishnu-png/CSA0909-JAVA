0a7aadbfacaaa4187d043a4af1460e05816fd8c2
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-23 20:15:22 +0530

    Create program43-hollow $ square.java

---
+++ program43-hollow $ square.java
@@ -0,0 +1,16 @@
+public class pattern{
+  public static void main(String[] args){
+    int i,j;
+    for(i = 1; i<=5; i++){
+      for(j = 1; j <=5; j++){
+        if(i == 1 || i == 5 || j == 1 || j == 5){
+          System.out.print("$ ");
+        }
+        else{
+          System.out.print("  ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
