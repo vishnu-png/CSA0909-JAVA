5f83dced657397d4a1c6205290e4cbb0d27abf8b
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-23 20:32:28 +0530

    Create program45-leapyear.java

---
+++ program45-leapyear.java
@@ -0,0 +1,11 @@
+public class leap{
+  public static void main(String[] args){
+    int n = 2023;
+    if(n%4 == 0){
+      System.out.print("it is leap year");
+    }
+    else{
+      System.out.print("it not a leap year");
+    }
+  }
+}
