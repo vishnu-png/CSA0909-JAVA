251177b5610889b4adb930cea54770f26c25f003
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-08-24 13:08:03 +0530

    Create upper case and lower case.java

---
+++ upper case and lower case.java
@@ -0,0 +1,10 @@
+public class Case{
+  public static void main(String[] args){
+    String input = "vishnuvardhan";
+    String u = input.toUpperCase();
+    String l = input.toLowerCase();
+    System.out.println(u);
+    System.out.println(l);
+   
+  }
+}
