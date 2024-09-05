6e9ead3514f51626ee12b883ef83b62c00643b23
vishnu vardhan kunda <132825249+vishnu-png@users.noreply.github.com>
2024-09-05 09:44:29 +0530

    Create m-convert Dtob,o.java

---
+++ m-convert Dtob,o.java
@@ -0,0 +1,9 @@
+public class R192224265{
+  public static void main(String[] args){
+    int d = 15;
+    String binary = Integer.toBinaryString(d);
+    String Oct = Integer.toOctalString(d);
+    System.out.println("binary "+binary);
+    System.out.println("octal "+Oct);
+  }
+}
